bf768b023dd95be67b6ddd65471cb44b57a11dd6
“Alexandre <alebarrionovo@gmail.com>
2023-12-07 12:44:23 -0300

    Projeto terraform 03

--- main.tf
+++ main.tf
@@ -1,12 +1,33 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "2.32.0"
     }
   }
 }
 
 provider "digitalocean" {
-  # Configuration options
+  token = var.do_token
+}
+
+variable "do_token" {
+
+}
+
+resource "digitalocean_droplet" "web" {
+  image    = "ubuntu-22-04-x64"
+  name     = "wevm-labs"
+  region   = "nyc1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [digitalocean_ssh_key.ssh.fingerprint]
+}
+
+resource "digitalocean_ssh_key" "ssh" {
+  name       = "Terraform"
+  public_key = file("~/Documents/digitalocean/terraform.pub")
+}
+
+output "ip" {
+  value = digitalocean_droplet.web.ipv4_address
 }
